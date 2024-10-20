c31c69dcf46c2ee9e6e25ddfd54cc76eebaa09ba
Artur <arturunkulov30@gmail.com>
2024-10-20 16:36:14 +0600

    code upadtaet

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+ALLOWED_HOSTS = ['212.112.118.74', 'value-label.onrender.com', '*']
+
+
 
 
 
